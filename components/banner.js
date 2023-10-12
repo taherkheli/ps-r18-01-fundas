46814ca100187ff4e0c6fb4a09f6874b7484b1a6
Bilal Taherkheli <Bilal.Taherkheli@laerdal.com>
2023-10-12 14:40:07 +0200

    apply some styling

--- components/banner.js
+++ components/banner.js
@@ -1,10 +1,10 @@
 const Banner = () => {
   return (
-    <header>
-      <div>
-        <img src="./GloboLogo.png" alt="logo" />
+    <header className="row mb-4">
+      <div className="col-5">
+        <img src="./GloboLogo.png" alt="logo" className="logo" />
       </div>
-      <div>Providing houses all over the world</div>
+      <div className="col-7 mt-5">Providing houses all over the world</div>
     </header>
   );
 };
