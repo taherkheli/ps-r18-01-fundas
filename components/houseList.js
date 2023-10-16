0b629e461e0d54daf001996c1e1e7bbbd3a3a0ce
Bilal Taherkheli <Bilal.Taherkheli@laerdal.com>
2023-10-16 14:50:34 +0200

    - we fake an async api and then fetch data (i.e. a side effect) inside a 'useEffect' hook - Side effects are operations or tasks that occur outside the regular rendering of a component, such as data fetching, DOM manipulation, or subscription management

--- components/houseList.js
+++ components/houseList.js
@@ -1,24 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import HouseRow from "./houseRow";
 
-const houseArray = [
-  {
-    id: 1,
-    address: "12 Valley of Kings, Geneva",
-    country: "Switzerland",
-    price: 900000,
-  },
-  {
-    id: 2,
-    address: "89 Road of Forks, Bern",
-    country: "Switzerland",
-    price: 500000,
-  },
-];
-
 const HouseList = () => {
-  // we never change houseArray. It is just provided as initial state. The live stateful representation is in 'houses' maintained by React itself i.e. 'houses' is read only
-  const [houses, setHouses] = useState(houseArray);  // houses = array representing current state; setHouses = function to change state
+  const [houses, setHouses] = useState([]);  // houses = array representing current state; setHouses = function to change state
+
+  useEffect( () => {
+    const fetchHouses = async () => {
+      const response = await fetch("/api/houses");
+      const houses = await response.json();
+      setHouses(houses);
+    };
+    fetchHouses();
+  });
 
   const addHouse = () => {
     setHouses([
