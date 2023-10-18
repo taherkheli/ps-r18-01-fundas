ac8153db37720f2c95cd205d6b61e11f31f5d5fd
Bilal Taherkheli <Bilal.Taherkheli@laerdal.com>
2023-10-18 15:37:08 +0200

    - consume a context in child component

--- components/app.js
+++ components/app.js
@@ -3,23 +3,23 @@ import Banner from "./banner";
 import navValues from "../helpers/navValues";
 import ComponentPicker from "./componentPicker";
 
-const navContext = React.createContext(navValues.home);
+const navigationContext = React.createContext(navValues.home);
 
 const App = () => {
-
-  const navigate = useCallback( (navTo) => {
-      setNav( { current: navTo, navigate } );
-    }, [] );
+  const navigate = useCallback(
+    (navTo, param) => setNav({ current: navTo, param, navigate }),
+    []
+  );
 
   const [ nav, setNav ] = useState( { current: navValues.home, navigate });
 
   return (
-    <navContext.Provider value={nav}>
+    <navigationContext.Provider value={nav}>
       <Banner headerText="Providing houses all over the world" />
       <ComponentPicker currentNavLocation={nav.current} />
-    </navContext.Provider>
+    </navigationContext.Provider>
   );
 };
 
-export { navContext };
+export { navigationContext };
 export default App;
--- components/houseRow.js
+++ components/houseRow.js
@@ -1,9 +1,13 @@
+import { useContext } from "react";
 import currencyFormatter from "../helpers/currencyFormatter";
+import navValues from "../helpers/navValues";
+import { navigationContext } from "./app";
 
-const HouseRow = ( { house } ) => {
+const HouseRow = ({ house }) => {
+  const { navigate } = useContext(navigationContext);
   return (
     <>
-      <tr onClick={ () => selectHouse(house) }>
+      <tr onClick={() => navigate(navValues.house, house)}>
         <td>{house.address}</td>
         <td>{house.country}</td>
         <td className={`${(house.price < 500000) ? "" : "text-primary"}`}>{currencyFormatter.format(house.price)}</td>
